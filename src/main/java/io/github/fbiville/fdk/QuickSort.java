817c008b64619af6caa19ae5920692a942a3de0d
Florent Biville <florent.biville@gmail.com>
2015-08-12 23:33:22 +0200

    Implement in-place Quick Sort

---
+++ src/main/java/io/github/fbiville/fdk/QuickSort.java
@@ -0,0 +1,81 @@
+package io.github.fbiville.fdk;
+
+public class QuickSort {
+
+    public static <T extends Comparable<T>> void sort(T[] values) {
+        sort(values, 0, values.length);
+    }
+
+    private static <T extends Comparable<T>> void sort(T[] values, int start, int end) {
+        if ((end - start) < 2) {
+            return;
+        }
+
+        if (end - start  == 2) {
+            if (values[start].compareTo(values[start+1]) > 0) {
+                swap(values, start, start+1);
+            }
+            return;
+        }
+
+        int pivotIndex = indexOfPivot(values, start, end);
+        swapPivot(values, start, pivotIndex);
+
+        int newPivotPosition = partitionAroundPivot(values, start, end);
+
+        sort(values, start, newPivotPosition);
+        sort(values, newPivotPosition+1, end);
+    }
+
+
+    private static <T extends Comparable<T>> int indexOfPivot(T[] values, int start, int end) {
+        T first = values[start];
+        T last = values[end - 1];
+        T middle = values[start + (end - start) / 2];
+
+        T median = median(first, last, middle);
+        if (first.compareTo(median) == 0) {
+            return start;
+        }
+        if (last.compareTo(median) == 0) {
+            return end - 1;
+        }
+        return start + (end - start) / 2;
+    }
+
+    private static <T extends Comparable<T>> void swapPivot(T[] values, int start, int position) {
+        if (position == start) {
+            return;
+        }
+        swap(values, start, position);
+    }
+
+    private static <T extends Comparable<T>> int partitionAroundPivot(T[] values, int start, int end) {
+        int currentBoundary = start;
+        T pivot = values[start];
+        for (int i = start + 1; i < end; i++) {
+            T current = values[i];
+            if (current.compareTo(pivot) <= 0) {
+                swap(values, i, ++currentBoundary);
+            }
+        }
+        swap(values, start, currentBoundary);
+        return currentBoundary;
+    }
+
+    private static <T extends Comparable<T>> void swap(T[] values, int firstIndex, int secondIndex) {
+        T temp = values[firstIndex];
+        values[firstIndex] = values[secondIndex];
+        values[secondIndex] = temp;
+    }
+
+    private static <T extends Comparable<T>> T median(T val1, T val2, T val3) {
+        if (val1.compareTo(val2) <= 0 && val2.compareTo(val3) <= 0) {
+            return val2;
+        }
+        if (val2.compareTo(val1) <= 0 && val1.compareTo(val3) <= 0) {
+            return val1;
+        }
+        return val3;
+    }
+}
---
+++ src/test/java/io/github/fbiville/fdk/QuickSortTest.java
@@ -0,0 +1,44 @@
+package io.github.fbiville.fdk;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+public class QuickSortTest {
+
+    @Test
+    public void returns_small_sorted_arrays_as_is() {
+        assertThat(sort(new Character[]{})).isEmpty();
+        assertThat(sort(new Character[]{'a'})).containsExactly('a');
+    }
+
+    @Test
+    public void sorts_arrays() {
+        assertThat(sort(new Integer[]{3, 2})).containsExactly(2,3);
+        assertThat(sort(new Integer[]{3, 2, 1})).containsExactly(1,2,3);
+        assertThat(sort(new Integer[]{2, 21, -1, 4, -6})).containsExactly(-6, -1, 2, 4, 21);
+    }
+
+    @Test(timeout = 10000)
+    public void sorts_long_arrays() {
+        Integer[] integers = random_array(10_000_000);
+
+        assertThat(sort(integers)).isSorted();
+    }
+
+    private Integer[] random_array(int length) {
+        Integer[] array = new Integer[length];
+        Random random = new Random(2*length);
+        for (int i = 0; i < length; i++) {
+            array[i] = random.nextInt(8*length);
+        }
+        return array;
+    }
+
+    private <T extends Comparable<T>> T[] sort(T[] values) {
+        QuickSort.sort(values);
+        return values;
+    }
+}
